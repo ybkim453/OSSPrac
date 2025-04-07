7dd8de1ee8275914139b096d7965944c4ecff551
YongBin Kim <yb1553@dgu.ac.kr>
2025-04-07 21:28:28 +0900

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 name = input("이름을 입력하세요: ")
 id = input("학번을 입력하세요: ")
 department = input("학과를 입력하세요: ")
+school = input("학교를 입력하세요: ")
 
 print("\n<출력>")
 print(f"이름: {name}")
 print(f"학번: {id}")
 print(f"학과: {department}")
+print(f"학교: {school}")
